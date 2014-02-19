4c88713b37c8f66b53d488d1069e4b06b1643204
crccheck <c@crccheck.com>
2014-02-19 00:03:58 -0600

    add simple cache

--- web.py
+++ web.py
@@ -8,6 +8,7 @@ import scrape
 
 
 app = Flask(__name__)
+cache = {}
 
 
 @app.route('/<city_name>/')
@@ -15,23 +16,29 @@ def city(city_name):
     url = os.environ.get(city_name.upper())
     if not url:
         abort(404)
-    response = requests.get(
-        url,
-        headers={
-            'User-Agent': 'bscrapeai/0.1',
-        },
-    )
-    data = scrape.find_data(response.text)
-    if response.status_code != 200:
-        abort(response.status_code)
-    output = {
-        'city': city_name,
-        'now': datetime.datetime.now(),
-        'result_length': len(data),
-        'results': data,
-    }
+    cache_key = url
+    now = datetime.datetime.now()
+    output = cache.get(cache_key)
+    if not output or now - output['now'] > datetime.timedelta(minutes=12):
+        response = requests.get(
+            url,
+            headers={
+                'User-Agent': 'bscrapeai/0.1',
+            },
+        )
+        if response.status_code != 200:
+            abort(response.status_code)
+        data = scrape.find_data(response.text)
+        output = {
+            'city': city_name,
+            'now': now,
+            'result_length': len(data),
+            'results': data,
+        }
+        cache[cache_key] = output
     return jsonify(output)
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run(host='0.0.0.0')
