0f9c45855c933223973aa3eabb8553d73768242b
crccheck <c@crccheck.com>
2014-02-18 23:36:54 -0600

    update flask to return result data

--- web.py
+++ web.py
@@ -1,10 +1,24 @@
-from flask import Flask
+import datetime
+
+from flask import Flask, jsonify
+
+import scrape
+
+
 app = Flask(__name__)
 
 
 @app.route('/<city_name>/')
 def city(city_name):
-    return 'Hello {}!'.format(city_name)
+    with open('test_data.txt', 'r') as f:
+        data = scrape.find_data(f.read())
+    output = {
+        'city': city_name,
+        'now': datetime.datetime.now(),
+        'result_length': len(data),
+        'results': data,
+    }
+    return jsonify(output)
 
 
 if __name__ == '__main__':
